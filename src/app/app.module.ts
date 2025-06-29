15cceae2c5057119903d8cbf9e1c21f9ada73101
rudraraul <rudraraul01@gmail.com>
2025-06-29 11:47:54 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { mobileComponent } from './shared/component/mobile/mobile.component'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    mobileComponent
+  
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/component/mobile/mobile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { Imobile } from "../moduls/mobile.interface";
+import { mobileProducts } from "../const/mobile";
+
+
+@Component ({
+    selector: 'app-mobile',
+    templateUrl: './mobile.component.html',
+    styleUrls: ['./mobile.component.scss']
+})
+export class mobileComponent {
+mobileArray : Array<Imobile>=  mobileProducts
+}
